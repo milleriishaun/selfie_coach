8973a360b78617b0a2296d31b6436017c6921d15
milleriishaun <32374007+milleriishaun@users.noreply.github.com>
2019-12-20 02:10:38 -0800

    Add model coach images from Unspash API

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const dataStore = require("nedb");
 const fetch = require("node-fetch");
 global.fetch = fetch;
 const app = express();
-const Unsplash = require("unsplash-js").default;
-const { toJson } = require("unsplash-js").default;
-
-const unsplash = new Unsplash({
-  accessKey: "{PICS}",
-  // Optionally if using a node-fetch polyfill or a version of fetch which supports the timeout option, you can configure the request timeout for all requests
-  timeout: 500 // values set in ms
-});
 
 app.listen(3000, () => console.log("listening on port 3000"));
-
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
@@ -22,26 +13,31 @@ database.loadDatabase();
 // database.insert({ firstName: "Cool", lastName: "Bro", status: "🤔" });
 // database.insert({ firstName: "Unbeareev", lastName: "Able", status: "🐻" });
 
-app.get("/api", (request, response) => {
+app.get("/api", (req, res) => {
+  fetch(
+    `https://api.unsplash.com/collections/155450/photos/?per_page=7&auto=format&w=200&dpi=2&client_id=65ac45f6f9404d5a86559b81a532d60b52c80e97921dfeaede0bbedddb7bb59e`
+  )
+    .then(response => {
+      console.log("response ok?: ", response.ok);
+      return response.json();
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(e => console.log("could not fetch from API, err: ", e));
+});
+
+app.get("/db", (req, res) => {
   database.find({}, (err, data) => {
     if (err) {
-      response.end();
+      res.end();
       return;
     }
-    response.json(data);
+    res.json(data);
   });
 });
 
-app.post("/api", (request, response) => {
-  // unsplash.search
-  //   .photos("selfie", 1, 10, { orientation: "portrait" })
-  //   .then(toJson)
-  //   .then(json => {
-  //     console.log("img src should change now");
-  //     document.getElementById("selfieCoach").src = json;
-  //   })
-  //   .catch(err, () => console.log("err from unsplash"));
-
+app.post("/db", (request, response) => {
   const serverData = request.body;
   const timestamp = Date.now();
   serverData.timestamp = timestamp;
--- public/client.js
+++ public/client.js
@@ -16,12 +16,55 @@ function setup() {
     () => console.log("Geolocation not available");
   }
 
+  const callFetch = fetch("/api")
+    .then(response => {
+      console.log("response ok?: ", response.ok);
+      return response.json();
+    })
+    .then(data => {
+      const unsplashJSON = JSON.stringify(data);
+      localStorage.setItem("unsplashModels", unsplashJSON);
+    });
+
+  const buttonCoach = document.getElementById("newCoach");
+  buttonCoach.addEventListener("click", event => {
+    const unsplashString = localStorage.getItem("unsplashModels");
+    const picArr = JSON.parse(unsplashString);
+    const quickMaths = Math.floor(Math.random(picArr.length) * picArr.length);
+    const oneRandomPic = picArr[quickMaths];
+    // const image = document.createElement("img");
+    // image.src = oneRandomPic.urls.thumb + ".png";
+    const image = document.getElementById("selfieCoach");
+    const altDescription = document.getElementById("altD");
+    image.src = oneRandomPic.urls.regular + ".png";
+    if (oneRandomPic.alt_description) {
+      altDescription.textContent = oneRandomPic.alt_description;
+    } else {
+      altDescription.textContent = "vogue";
+    }
+    // root.append(image);
+    // document.body.append(root);
+    // for (item of picArr) {
+    //   const image = document.createElement("img");
+    //   image.src = item.urls.thumb + ".png";
+    //   if (item.alt_description) {
+    //     image.alt = item.alt_description;
+    //   } else {
+    //     image.alt = "vogue";
+    //   }
+    //   console.log("image: ", image);
+    //   root.append(image);
+    //   document.body.append(root);
+    // }
+  });
+
   const button = document.getElementById("submit");
   button.addEventListener("click", async event => {
     video.loadPixels();
+    const coachPose = document.getElementById("selfieCoach").src;
     const mood = document.getElementById("mood").value;
     const image64 = video.canvas.toDataURL();
-    const data = { lat, lon, mood, image64 };
+    const data = { lat, lon, mood, image64, coachPose };
     const options = {
       method: "POST",
       headers: {
@@ -29,8 +72,8 @@ function setup() {
       },
       body: JSON.stringify(data)
     };
-    const response = await fetch("/api", options);
-    const json = await response.json();
-    console.log(json);
+    const response = await fetch("/db", options);
+    const json2 = await response.json();
+    console.log("submitButton json2: ", json2);
   });
 }
