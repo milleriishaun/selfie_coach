e7060e524f926da2ae85d285044bb32dbe9f45e3
milleriishaun <32374007+milleriishaun@users.noreply.github.com>
2020-01-01 14:50:35 -0800

    Heroku3

--- start-client.js
+++ start-client.js
@@ -1,3 +1,5 @@
 const args = ["start"];
 const opts = { stdio: "inherit", cwd: "public", shell: true };
 require("child_process").spawn("npm", args, opts);
+
+//"heroku-postbuild": "cd client && npm install && npm install --only=dev --no-shrinkwrap && npm run build"
